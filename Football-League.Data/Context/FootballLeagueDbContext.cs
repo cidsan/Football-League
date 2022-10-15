8a8ab29aa3d37d096896656ceb027a72af910100
Stiliyan Kostadinov <st.k.kostadinov@gmail.com>
2022-10-15 18:59:38 +0300

    Initial commit

---
+++ Football-League.Data/Context/FootballLeagueDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football_League.Data.Context
+{
+    public class FootballLeagueDbContext : DbContext
+    {
+        public FootballLeagueDbContext(DbContextOptions<FootballLeagueDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
