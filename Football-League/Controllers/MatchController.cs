f8167d4743dbdfcb0083eb42d539e780343c1a68
Stiliyan Kostadinov <st.k.kostadinov@gmail.com>
2022-10-18 09:32:07 +0300

    Added controllers for team and match

---
+++ Football-League/Controllers/MatchController.cs
@@ -0,0 +1,52 @@
+﻿using Football_League.Data.Models;
+using Football_League.Services.MatchService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Football_League.Controllers
+{
+    [Route("api/match")]
+    [ApiController]
+    public class MatchController : ControllerBase
+    {
+        private readonly IMatchService _matchService;
+
+        public MatchController(IMatchService matchService)
+        {
+            _matchService = matchService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Match>> GetMatch(int id)
+        {
+            return Ok(await _matchService.GetAsync(id));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Match>>> GetAllMatches()
+        {
+            return Ok(await _matchService.GetAllAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Match>> CreateMatch(Match match)
+        {
+            return Ok(await _matchService.CreateAsync(match));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteMatch(int id)
+        {
+            await _matchService.DeleteAsync(id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Match>> StartMatch(Match match)
+        {
+            return Ok(await _matchService.StartMatchAsync(match));
+        }
+    }
+}
